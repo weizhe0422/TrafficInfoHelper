ad2d08415c2f70277a16d6df3d1ea6ceceac0b96
WeiZhe Chang <weizhe.chang@gmail.com>
2019-03-24 23:46:57 +0800

    add @CPBL

--- events/event.go
+++ events/event.go
@@ -3,5 +3,5 @@ package events
 import "github.com/line/line-bot-sdk-go/linebot"
 
 func EventCPBL(client *linebot.Client, event *linebot.Event) {
-	client.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("you need CPBL info"))
+	client.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("you need CPBL info")).Do()
 }

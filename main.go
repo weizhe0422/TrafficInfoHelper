3e05873ab61197955c0d43976a8ecdd859592170
WeiZhe Chang <weizhe.chang@gmail.com>
2019-03-24 23:49:39 +0800

    add @CPBL

--- main.go
+++ main.go
@@ -55,6 +55,7 @@ func handleEvent(resp http.ResponseWriter, req *http.Request) {
 			case *linebot.TextMessage:
 				switch message.Text {
 				case "@CPBL":
+					G_lineBotclient.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("@CPBL")).Do()
 					botEvents.EventCPBL(G_lineBotclient, event)
 				}
 				G_lineBotclient.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do()

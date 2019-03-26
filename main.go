09bd0d9a4d73f906779a8c5eb00df455d7965057
WeiZhe Chang <weizhe.chang@gmail.com>
2019-03-27 00:20:39 +0800

    add THSR function

--- main.go
+++ main.go
@@ -55,6 +55,7 @@ func handleEvent(resp http.ResponseWriter, req *http.Request) {
 			case *linebot.TextMessage:
 				switch message.Text {
 				case "@THSR":
+					G_lineBotclient.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("XXXX")).Do()
 					botEvents.EventTHSR(G_lineBotclient, event)
 				}
 				G_lineBotclient.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do()
